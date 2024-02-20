c38fcdf3e185e03a33fa357a4e6668aeffe4f431
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-02-20 00:08:30 +0000

    }

---
+++ EXAMPLES/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){  
+        System.out.println("desde el main");
+        Car c = new Car();
+        c.pitar();
+        
+    }
+}
+
