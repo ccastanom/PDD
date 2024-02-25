2053848c2478cece14f881f9c40b3e8478a506d4
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-02-25 01:39:06 +0000

    Cambios 24/02/2024

---
+++ ANIMALES/MainAnimales.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void Main (String [] args){
+        Sistem.out.println("desde el main");
+        
+
+
+
+    }
+
+}
+
+
+public class Main {
+    public static void main (String[] args){  
+        System.out.println("desde el main");
+        Car c = new Car();
+        c.pitar();
+        
+    }
+}
