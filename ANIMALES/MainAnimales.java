b564b3339fd0ea3fc55a3a8e2cda10067bb83cd8
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-02-25 01:58:56 +0000

    Cambios 24/02/2024

--- ANIMALES/MainAnimales.java
+++ ANIMALES/MainAnimales.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void Main (String [] args){
+public class MainAnimales {
+    public static void MainAnimales (String [] args){
         Sistem.out.println("desde el main");
         //Car c = new Car();
         //c.pitar();
---
+++ ANIMALES/Vertebrados.java
@@ -0,0 +1,20 @@
+abstract public class Vertebrados extends Animales { 
+    //atributos generales de la clase
+public camuflaje;
+public velocidad;
+
+
+//Constructor de la clase vertebrados
+public Vertebrados {
+ Sistem.out.printLn("constuctor de un vertebrado");
+ }
+
+//Metodos abstractos de la clase vertebrados (aun no se pueden definir)
+abstract public void alimentacion 
+abstract public void habitat 
+
+} 
+//Atributos comunes para una clase de animales herbívoros:  Tipo de alimentación:
+//Ramoneo: Consumen hojas, brotes y ramas de plantas leñosas.
+//Pastoreo: Consumen hierbas y plantas de bajo crecimiento.
+//Frutos y semillas: Consumen principalmente frutos y semillas de plantas.
