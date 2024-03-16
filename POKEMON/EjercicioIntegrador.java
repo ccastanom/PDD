a5c880c189f6445eafe4aeb231b1dbcc202b3b82
ccastanom <159740202+ccastanom@users.noreply.github.com>
2024-03-16 20:34:44 +0000

    Pokemon 16/03/2024

---
+++ POKEMON/EjercicioIntegrador.java
@@ -0,0 +1,9 @@
+
+public class EjercicioIntegrador {
+
+    public static void main(String[] args){
+
+
+    }
+
+}
---
+++ POKEMON/Pokemon.java
@@ -0,0 +1,12 @@
+
+public abstract class Pokemon {
+
+    protected int numPokedex;
+    protected String nombre;
+    protected double peso;
+    protected String sexo;
+    protected int temporada;
+    
+
+
+}
